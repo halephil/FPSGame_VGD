7be1be9b836f98054831c2e081a20fb32de4fff4
halephil <halen.s.phillips@gmail.com>
2017-04-28 17:21:34 -0500

    Added Final Boss
    -Dino Makes noises when hit
    -Dino Attack if in certain proximity
    -Dino Makes a noise when dies
    -Dino Follows player around map

---
+++ FPSGame_VGD/Assets/Scripts/HealthTracker.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour {
+
+    public AudioClip deathSound;
+    public AudioClip hurtSound;
+    public AudioClip attackSound;
+    private int health;
+    private Animation anim;
+    
+	// Use this for initialization
+	void Start () {
+        health = 1000;
+        anim = gameObject.GetComponent<Animation>();
+        GetComponent<AudioSource>().playOnAwake = false;
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CheckHealth();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "FPSController")
+        {
+            anim.CrossFade("Attack");
+            anim.PlayQueued("Walk");
+            GetComponent<AudioSource>().clip = attackSound;
+            GetComponent<AudioSource>().Play();
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "Bullet(Clone)")
+        {
+            health = health - 5;
+            AfterHit();
+        }
+
+        else if (collision.gameObject.name == "Rocket(Clone)")
+        {
+            health = health - 20;
+            AfterHit();
+        }
+
+        else if (collision.gameObject.name == "Grenade(Clone)")
+        {
+            health = health - 10;
+            AfterHit();
+        }
+        else
+        {
+            
+        }
+
+        if(health <= 0)
+        {
+            anim.CrossFade("Stand Die");
+            GetComponent<AudioSource>().clip = deathSound;
+            GetComponent<AudioSource>().Play();
+        }
+        
+       
+
+    }
+    private void AfterHit()
+    {
+       
+        anim.Play("Hurt");
+        anim.PlayQueued("Walk");
+        Debug.Log(health);
+        if (GetComponent<AudioSource>().isPlaying == false)
+        {
+            GetComponent<AudioSource>().clip = hurtSound;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+    void CheckHealth()
+    {
+        if(health <= 0)
+        {
+            if (anim.IsPlaying("Stand Die") == false)
+            {
+                Destroy(gameObject);
+            }
+            
+          
+        }
+    }
+
+
+}
+
